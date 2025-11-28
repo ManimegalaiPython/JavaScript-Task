c10d60bdce38d3180008635b8bf4b5e92082e060
Manimegalai <118519630+ManimegalaiPython@users.noreply.github.com>
2025-11-28 13:32:41 +0530

    Add files via upload
    Tasks

---
+++ Javascript/Day1-Task/script.js
@@ -0,0 +1,16 @@
+
+
+ // Task-2: Create variables
+var a = 10;
+let b = 20;
+const c = 30;
+
+// Print in console
+console.log("var a =", a);
+console.log("let b =", b);
+console.log("const c =", c);
+
+// Print in browser inside HTML spans
+document.getElementById("varValue").innerText = a;
+document.getElementById("letValue").innerText = b;
+document.getElementById("constValue").innerText = c;
